4334c8a81664612c8626fd3f25637a98445c3e49
MrHyde <diego.ospinagp@gmail.com>
2022-02-08 21:02:34 -0500

    feat: added authentication system with Laravel UI package

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Authentication
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
