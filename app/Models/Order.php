71ce85d26b77c7c1628cfea16600a12b25779512
MrHyde <diego.ospinagp@gmail.com>
2022-02-08 21:47:45 -0500

    feat: created order model and migration with custom fields and relations

---
+++ app/Models/Order.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    /**
+     * Get the user that belongs the order
+     * 
+     * @return [type]
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2022_02_09_021349_create_orders_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            
+            $table->string('customer_name', 80);
+            $table->string('customer_email', 120);
+            $table->string('customer_mobile', 40);
+            $table->string('status', 20);
+            $table->unsignedInteger('user_id');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
