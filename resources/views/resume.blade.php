56dde0cb7d741370f9e79a6e243fc9fb609c64c2
MrHyde <diego.ospinagp@gmail.com>
2022-02-10 13:31:06 -0500

    feat: created resume view

---
+++ resources/views/resume.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>thank you</title>
+</head>
+<body>
+    <div class="container">
+        <div class="grid grid-cols-3 grid-rows-3 justify-center items-center">
+            <div class="col-start-2 row-start-2 flex flex-col justify-center items-center">
+                <table>
+                    <thead>
+                        <th class="border px-4 py-2">Transacción aprobada</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="border px-4 py-2">Referencia: 306</td>
+                        </tr>
+                        <tr>
+                            <td class="border px-4 py-2">Producto: USB Big Enter Key Table Pillow for Angry Developers</td>
+                        </tr>
+                        <tr>
+                            <td class="border px-4 py-2">Valor: $500.00</td>
+                        </tr>
+                        <tr>
+                            <td class="border px-4 py-2">Muchas gracias por su compra.</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-start-2 row-start-3 text-center bg-lime-500 hover:bg-lime-400 text-white font-bold py-2 px-4 rounded-full">
+                <button type="button">Volver a la tienda</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
