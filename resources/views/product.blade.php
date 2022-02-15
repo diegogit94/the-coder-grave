2ac69a96a1c834850e4a9f2e13b292b5acf07e07
MrHyde <diego.ospinagp@gmail.com>
2022-02-14 20:57:31 -0500

    view: modified product view to fill some fields with user DB information

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -11,25 +11,27 @@
     <div class="container">
         <div class="grid grid-cols-3 grid-rows-1 justify-center items-center py-2">
             <div class="col-start-2 row-start-1 flex flex-col justify-center items-center">
-                <table>
-                    <thead>
-                        <th class="border px-4 py-2">USB Big Enter Key Table Pillow for Angry Developers</th>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td class="border px-4 py-2"><img src="https://55gadgets.com/wp-content/uploads/2018/06/USB-Big-Enter-Key-Table-Pillow.jpg" alt=""></td>
-                        </tr>
-                        <tr>
-                            <td class="border px-4 py-2">¿Te dan ganas golpear algo despues de googlear esos mil errores mientras programas? Esta almohada es para ti ;)</td>
-                        </tr>
-                        <tr>
-                            <td class="border px-4 py-2">¡¡ Puedes tenerla por solo $500.00 !!</td>
-                        </tr>
-                        <tr>
-                            <td class="border px-4 py-2 text-center"><button class="button bg-lime-500 hover:bg-lime-400 text-white font-bold py-2 px-4 rounded-full">Comprar</button></td>
-                        </tr>
-                    </tbody>
-                </table>
+                <form method="GET" action="{{ route('checkout.index') }}">
+                    <table>
+                        <thead>
+                            <th class="border px-4 py-2">{{ $product->name }}</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="border px-4 py-2"><img src="{{ $product->image }}" alt=""></td>
+                            </tr>
+                            <tr>
+                                <td class="border px-4 py-2">¿Te dan ganas golpear algo despues de googlear esos mil errores mientras programas? Esta almohada es para ti ;)</td>
+                            </tr>
+                            <tr>
+                                <td class="border px-4 py-2">¡¡ Puedes tenerla por solo ${{ $product->price }}.00 !!</td>
+                            </tr>
+                            <tr>
+                                <td class="border px-4 py-2 text-center"><button type="submit" class="button bg-lime-500 hover:bg-lime-400 text-white font-bold py-2 px-4 rounded-full">Comprar</button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </form>
             </div>
         </div>
     </div>
