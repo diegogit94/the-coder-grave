b3d78b60a03c4f5fe8ce8d7b81ba0312a0e2907f
MrHyde <diego.ospinagp@gmail.com>
2022-02-15 21:19:21 -0500

    feat: changed logic when is create a new register in orders table

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -36,21 +36,24 @@ class CheckoutController extends Controller
      */
     public function pay(Request $request, Product $product)
     {
-        $connection = new PlacetopayConnection();
-        $response = $connection->createRequest($request, $product);
-
         $order = new Order;
 
         $order->customer_name = auth()->user()->name;
         $order->customer_email = auth()->user()->email;
         $order->customer_mobile = auth()->user()->mobile;
         $order->total = $product->price;
-        $order->status = 'pending';
-        $order->request_id = $response['requestId'];
+        $order->status = 'CREATED';
         $order->user_id = auth()->user()->id;
         $order->product_id = $product->id;
-
+        
         $order->save();
+
+        $connection = new PlacetopayConnection();
+        $response = $connection->createRequest($request, $product, $order);
+
+        $order->request_id = $response['requestId'];
+
+        $order->update();
 
         return redirect($response['processUrl']);
     }
